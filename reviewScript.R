3be3a0f1ceae8f1cf7e9055627f2fe8e053effe0
fmlove <fminl@comcast.net>
2016-12-20 16:36:46 +0000

    review function now successfully retrieves a user's skids from a particular date; duplicate soma check split off into separate funtion

--- reviewScript.R
+++ reviewScript.R
@@ -1,10 +1,19 @@
-catmaidCheck <- function(dateSince, user = 'all'){#date format as YYY-MM-DD string or a date value
-  #if (!is.date(dateSince)){
-    since = as.Date(dateSince)
-  #}
-  #else{
-  #  since = dateSince
-  #}
+checkMine <- function(dateSince, user = 'all'){#date format as YYYMMDD string, user as login name
   
+  if (user != 'all'){
+    ul = catmaid_get_user_list()
+    uid = ul[ul$login == user,]$id #there's probably a much better way of doing this...
+    userparam = paste("&created_by=", uid, sep = '')
+  }
+  else{
+    userparam = ''
+  }
+  
+  mySkeletons = as.integer(catmaid_fetch(paste("/1/skeletons/?nodecount_gt=1&from=", dateSince, userparam, sep = '')))
+  
+  
+}
+
+duplicateSomas <- function(){
   
 }
