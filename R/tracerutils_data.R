8a0f1e4e2e6ce6dec64fbeb68b718bff97a75545
fmlove <fminl@comcast.net>
2017-11-15 17:24:24 +0000

    Documentation updates

--- R/tracerutils_data.R
+++ R/tracerutils_data.R
@@ -2,7 +2,7 @@
 #'
 #' @name glomerular.valence
 #' @docType data
-#' @description A named character vector containing the functional valence of each antennal lobe glomerulus
+#' @format A named character vector containing the functional valence of each antennal lobe glomerulus
 #'  (as described in Paavo Huoviala's thesis).  Possible valences are "Attractive", "Aversive, "Pheromonal",
 #'  "Egg_laying", "Food", and "Unknown".  Element names correspond to the names of glomeruli.
 NULL
