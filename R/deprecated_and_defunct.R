b1bb6bd941078894a46c40fd4c27eb2e1573b886
fmlove <fminl@comcast.net>
2018-02-28 09:50:52 +0000

    Making quick_nblast defunct and creating new file for removed functions

---
+++ R/deprecated_and_defunct.R
@@ -0,0 +1,21 @@
+#-----REMOVED FUNCTIONS-----
+
+#' @name tracerutils-defunct
+NULL
+
+
+#' NBLAST a CATMAID neuron against the flycircuit data set
+#'
+#' DEFUNCT - use \code{\link{nblast_fafb}}
+#'
+#' Requires the \code{flycircuit.datadir} option to be set and the \code{nat.default.neuronlist} option to be set to "dps" in .Rprofile
+#'
+#' @rdname tracerutils-defunct
+#'
+#' @param skid The skeleton ID of the neuron in CATMAID
+#' @return The full NBLAST results object
+#'
+#' @export
+quick_nblast <- function(skid){
+  .Defunct("elmr::nblast_fafb", package = "tracerutils", "This method has been removed; please use elmr::nblast_fafb instead.")
+}
--- R/utils.R
+++ R/utils.R
@@ -1,36 +1,5 @@
 #-----UTILITIES-----
 
-#' NBLAST a CATMAID neuron against the flycircuit data set
-#'
-#' NBLAST a neuron in CATMAID (FAFB) against the full flycircuit neuron list
-#'
-#' Requires the \code{flycircuit.datadir} option to be set and the \code{nat.default.neuronlist} option to be set to "dps" in .Rprofile
-#'
-#' @rdname tracerutils-deprecated
-#'
-#' @param skid The skeleton ID of the neuron in CATMAID
-#' @return The full NBLAST results object
-#'
-#' @export
-#'
-quick_nblast <- function(skid){
-  .Deprecated("nblast_fafb", "elmr", "quick_nblast is deprecated, please use elmr::nblast_fafb instead")
-
-  if(!requireNamespace("doMC", quietly = TRUE)){
-    warning("The NBLAST search will not be parallelised without the 'doMC' package installed.  This might take a long time to complete.")
-  }
-  else{
-    doMC::registerDoMC(4)
-  }
-
-  dps <<- nat::read.neuronlistfh(getOption("flycircuit.datadir"))
-
-  library(nat.nblast)# workaround to nat.nblast issue #31
-  results = elmr::nblast_fafb(skid)
-}
-
-
-
 #' Split a neuron and return 'downstream' section
 #'
 #' Split a neuron at a particular node and return the 'downstream' section as a
